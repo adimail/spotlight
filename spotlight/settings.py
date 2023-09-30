c04e0465bf95916293ea1799b0296541d593d0d1
Aditya <adimail2404@gmail.com>
2023-09-30 17:06:11 +0530

    title icon

--- spotlight/settings.py
+++ spotlight/settings.py
@@ -118,9 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
